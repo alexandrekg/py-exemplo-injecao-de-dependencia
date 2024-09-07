2d13ce0906f41861dd9e3c5fba6ae893adcf8afc
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-09-07 10:25:24 -0300

    Criando classe Pessoa para exemplo

---
+++ main.py
@@ -0,0 +1,17 @@
+class Pessoa:
+    def correr(self):
+        print('Estou correndo')
+
+    def falar(self):
+        print('Estou falando')
+
+    def comer(self):
+        print('Comendo uma pizza')
+
+    def jogarFutebol(self):
+        print('*Jogar Futebol*')
+
+pessoa = Pessoa()
+pessoa.correr()
+pessoa.falar()
+pessoa.comer()
